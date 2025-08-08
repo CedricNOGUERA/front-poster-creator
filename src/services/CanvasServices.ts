d59978c91d0d9d4e6fc8d6b826b7da7fc07d45a3
ffolituu <fabrice.folituu@lwane.com>
2025-08-08 08:50:36 -1000

    refactor: replace the raw base url with that of the environment file

--- src/services/CanvasServices.ts
+++ src/services/CanvasServices.ts
@@ -1,31 +1,33 @@
 import { Canvastype } from "@/types/CanvasType";
 
-const API_URL = import.meta.env.VITE_API_URL
-
 class CanvasService {
-
-    async postCanvas(newCanvas: Canvastype) {
-    const response = await fetch('http://localhost:8081/api/add-canvas', {
-        method: 'POST',
+  async postCanvas(newCanvas: Canvastype) {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/add-canvas`,
+      {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(newCanvas),
-      });
-      return response
-    }
+      }
+    );
+    return response;
+  }
 
-    async deleteCanvas(canvasId: string){
-      const response = await fetch(`${API_URL}/api/canvases/${canvasId}`, {
-        method: 'DELETE',
+  async deleteCanvas(canvasId: string) {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/canvases/${canvasId}`,
+      {
+        method: "DELETE",
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
         },
-      })
-      return response
-    }
-
+      }
+    );
+    return response;
+  }
 }
 
 const canvasServiceInstance = new CanvasService();
