8d8f9b10773034ff599ee2630a0f8605c04d5d1c
CedricNOGUERA <cedric.noguera@lwane.com>
2025-09-17 16:23:53 -1000

    feat: button back to

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -1,94 +1,79 @@
-import useStoreApp from "@/stores/storeApp";
+import useStoreApp from '@/stores/storeApp'
+import { ResetConfig } from '@/types/DiversType'
+import { Button } from 'react-bootstrap'
 
 export const ProgressBar = () => {
   /* States
    *******************************************************************************************/
-  const storeApp = useStoreApp();
+  const storeApp = useStoreApp()
+
+  const resetConfig: ResetConfig = {
+    1: ['shopId', 'categoryId', 'templateId', 'dimensionId'],
+    2: ['categoryId', 'templateId', 'dimensionId'],
+    3: ['templateId', 'dimensionId'],
+    4: ['dimensionId'],
+    5: [],
+  }
+
+  const goToStep = (step: number) => {
+    storeApp.setStep(step)
+
+    resetConfig[step].forEach((key) => {
+      if (key === 'shopId') storeApp.setShopId(0)
+      if (key === 'categoryId') storeApp.setCategoryId(0)
+      if (key === 'templateId') storeApp.setTemplateId(0)
+      if (key === 'dimensionId') storeApp.setDimensionId(0)
+    })
+  }
 
   /* Render
    *******************************************************************************************/
   return (
     <div className={`progress-bar bg-light border-bottom`}>
+      {storeApp.step > 1 && (
+        <Button
+          variant='link'
+          className='p-0 me-3 text-muted'
+          onClick={() => goToStep(storeApp.step - 1)}
+          style={{ textDecoration: 'none' }}
+        >
+          <i className='fa fa-circle-arrow-left fs-3'></i>
+        </Button>
+      )}
       <div
-        className={`step ${storeApp.step >= 1 ? "current" : ""}`}
-        onClick={() => {
-          storeApp.setStep(1);
-          storeApp.setShopId(0);
-          storeApp.setCategoryId(0);
-          storeApp.setTemplateId(0);
-          storeApp.setDimensionId(0);
-        }}
+        className={`step ${storeApp.step >= 1 ? 'current' : ''}`}
+        onClick={() => goToStep(1)}
       >
-        {storeApp.step > 1 ? (
-          <i className="fas fa-check text-white"></i>
-        ) : (
-          <>1</>
-        )}
-        <div className="progress-line"></div>
+        {storeApp.step > 1 ? <i className='fas fa-check text-white'></i> : <>1</>}
+        <div className='progress-line'></div>
       </div>
       <div
-        className={`step ${storeApp.step >= 2 ? "current" : ""}`}
-        onClick={
-          storeApp.shopId !== 0
-            ? () => {
-                storeApp.setStep(2);
-                storeApp.setCategoryId(0);
-                storeApp.setTemplateId(0);
-                storeApp.setDimensionId(0);
-              }
-            : undefined
-        }
+        className={`step ${storeApp.step >= 2 ? 'current' : ''}`}
+        onClick={storeApp.shopId !== 0 ? () => goToStep(2) : undefined}
       >
-        {storeApp.step > 2 ? (
-          <i className="fas fa-check text-white"></i>
-        ) : (
-          <>2</>
-        )}
-        <div className="progress-line"></div>
+        {storeApp.step > 2 ? <i className='fas fa-check text-white'></i> : <>2</>}
+        <div className='progress-line'></div>
       </div>
       <div
-        className={`step ${storeApp.step >= 3 ? "current" : ""}`}
-        onClick={
-          storeApp.categoryId !== 0
-            ? () => {
-                storeApp.setStep(3);
-                storeApp.setTemplateId(0);
-                storeApp.setDimensionId(0);
-              }
-            : undefined
-        }
+        className={`step ${storeApp.step >= 3 ? 'current' : ''}`}
+        onClick={storeApp.categoryId !== 0 ? () => goToStep(3) : undefined}
       >
-        {storeApp.step > 3 ? (
-          <i className="fas fa-check text-white"></i>
-        ) : (
-          <>3</>
-        )}
-        <div className="progress-line"></div>
+        {storeApp.step > 3 ? <i className='fas fa-check text-white'></i> : <>3</>}
+        <div className='progress-line'></div>
       </div>
       <div
-        className={`step ${storeApp.step >= 4 ? "current" : ""}`}
-        onClick={
-          storeApp.templateId !== 0
-            ? () => {
-                storeApp.setStep(4);
-                storeApp.setDimensionId(0);
-              }
-            : undefined
-        }
+        className={`step ${storeApp.step >= 4 ? 'current' : ''}`}
+        onClick={storeApp.templateId !== 0 ? () => goToStep(4) : undefined}
       >
-        {storeApp.step > 4 ? (
-          <i className="fas fa-check text-white"></i>
-        ) : (
-          <>4</>
-        )}
-        <div className="progress-line"></div>
+        {storeApp.step > 4 ? <i className='fas fa-check text-white'></i> : <>4</>}
+        <div className='progress-line'></div>
       </div>
       <div
-        className={`step ${storeApp.step >= 5 ? "current" : ""}`}
+        className={`step ${storeApp.step >= 5 ? 'current' : ''}`}
         onClick={
           storeApp.dimensionId !== 0
             ? () => {
-                storeApp.setStep(5);
+                storeApp.setStep(5)
               }
             : undefined
         }
@@ -96,5 +81,5 @@ export const ProgressBar = () => {
         5
       </div>
     </div>
-  );
-};
+  )
+}
--- src/components/common/ProgressBarDrag.tsx
+++ src/components/common/ProgressBarDrag.tsx
@@ -1,6 +1,8 @@
-import useStoreApp from "@/stores/storeApp";
-import React from "react";
-import { useOutletContext } from "react-router-dom";
+import useStoreApp from '@/stores/storeApp'
+import { ResetConfig } from '@/types/DiversType'
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useOutletContext } from 'react-router-dom'
 
 interface ProgressBarContextType {
   setHasModel: React.Dispatch<React.SetStateAction<boolean>>
@@ -9,103 +11,66 @@ interface ProgressBarContextType {
 export const ProgressBarDrag = () => {
   /* States
    *******************************************************************************************/
-  const { setHasModel } =
-    useOutletContext<ProgressBarContextType>()
-  const storeApp = useStoreApp();
+  const { setHasModel } = useOutletContext<ProgressBarContextType>()
+  const storeApp = useStoreApp()
+
+  const resetConfig: ResetConfig = {
+    1: ['shopId', 'categoryId', 'templateId', 'dimensionId'],
+    2: ['categoryId', 'templateId', 'dimensionId'],
+    3: ['templateId', 'dimensionId'],
+    4: ['dimensionId'],
+    5: [],
+  }
+
+  const goToStep = (step: number) => {
+    storeApp.setStep(step)
+
+    resetConfig[step].forEach((key) => {
+      if (key === 'shopId') storeApp.setShopId(0)
+      if (key === 'categoryId') storeApp.setCategoryId(0)
+      if (key === 'templateId') storeApp.setTemplateId(0)
+      if (key === 'dimensionId') storeApp.setDimensionId(0)
+    })
+  }
 
   /* Render
    *******************************************************************************************/
   return (
-    <div
-      className={`progress-bar bg-light border-bottom`}
-    >
+    <div className={`progress-bar bg-light border-bottom`}>
+      {storeApp.step > 1 && (
+        <Button
+          variant='link'
+          className='p-0 me-3 text-muted'
+          onClick={() => goToStep(storeApp.step - 1)}
+          style={{ textDecoration: 'none' }}
+        >
+          <i className='fa fa-circle-arrow-left fs-3'></i>
+        </Button>
+      )}
       <div
-        className={`step ${storeApp.step >= 1 ? "current" : ""}`}
-        onClick={() => {
-          storeApp.setStep(1);
-          storeApp.setShopId(0);
-          storeApp.setCategoryId(0);
-          storeApp.setTemplateId(0);
-          storeApp.setDimensionId(0);
-        }}
+        className={`step ${storeApp.step >= 1 ? 'current' : ''}`}
+        onClick={() => goToStep(1)}
       >
-        {storeApp.step > 1 ? (
-          <i className="fas fa-check text-white"></i>
-        ) : (
-          <>1</>
-        )}
-        <div className="progress-line"></div>
+        {storeApp.step > 1 ? <i className='fas fa-check text-white'></i> : <>1</>}
+        <div className='progress-line'></div>
       </div>
       <div
-        className={`step ${storeApp.step >= 2 ? "current" : ""}`}
+        className={`step ${storeApp.step >= 2 ? 'current' : ''}`}
         onClick={
           storeApp.shopId !== 0
             ? () => {
-                storeApp.setStep(2);
-                storeApp.setCategoryId(0);
-                storeApp.setTemplateId(0);
-                storeApp.setDimensionId(0);
+                goToStep(2)
                 setHasModel(false)
               }
             : undefined
         }
       >
-        {storeApp.step > 2 ? (
-          <i className="fas fa-check text-white"></i>
-        ) : (
-          <>2</>
-        )}
-        <div className="progress-line"></div>
+        {storeApp.step > 2 ? <i className='fas fa-check text-white'></i> : <>2</>}
+        <div className='progress-line'></div>
       </div>
-      <div
-        className={`step ${storeApp.step >= 3 ? "current" : ""}`}
-        onClick={
-          storeApp.categoryId !== 0
-            ? () => {
-                storeApp.setStep(3);
-                storeApp.setTemplateId(0);
-                storeApp.setDimensionId(0);
-              }
-            : undefined
-        }
-      >
-        {storeApp.step > 3 ? (
-          <i className="fas fa-check text-white"></i>
-        ) : (
-          <>3</>
-        )}
-        {/* <div className="progress-line"></div> */}
+      <div className={`step ${storeApp.step >= 3 ? 'current' : ''}`}>
+        {storeApp.step > 3 ? <i className='fas fa-check text-white'></i> : <>3</>}
       </div>
-      {/* <div
-        className={`step ${storeApp.step >= 4 ? "current" : ""}`}
-        onClick={
-          storeApp.templateId !== 0
-            ? () => {
-                storeApp.setStep(4);
-                storeApp.setDimensionId(0);
-              }
-            : undefined
-        }
-      > */}
-        {/* {storeApp.step > 4 ? (
-          <i className="fas fa-check text-white"></i>
-        ) : (
-          <>4</>
-        )}
-        <div className="progress-line"></div>
-      </div>
-      <div
-        className={`step ${storeApp.step >= 5 ? "current" : ""}`}
-        onClick={
-          storeApp.dimensionId !== 0
-            ? () => {
-                storeApp.setStep(5);
-              }
-            : undefined
-        }
-      >
-        5*/}
-      {/* </div>  */}
     </div>
-  );
-};
+  )
+}
