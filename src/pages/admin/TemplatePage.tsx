07c5dddc64b15cc1a7c75a46797b957c3de29f12
CedricNOGUERA <cedric.noguera@lwane.com>
2026-01-27 16:55:30 -1000

    feat: TemplatePage

--- src/pages/admin/TemplatePage.tsx
+++ src/pages/admin/TemplatePage.tsx
@@ -5,7 +5,7 @@ import { ShopType } from "@/types/ShopType";
 import { TemplateType } from "@/types/TemplatesType";
 import { _deleteModel, _deleteModels, _getAllImagesModels, _getImagesModels, _getModels, _getTemplates } from "@/utils/apiFunctions";
 import React from "react";
-import { Button, Col, Container, Dropdown, Image, Modal, Row, Spinner, Table } from "react-bootstrap";
+import { Alert, Button, Col, Container, Dropdown, Image, Modal, Row, Spinner, Table } from "react-bootstrap";
 import { FaTimesCircle } from "react-icons/fa";
 import { FaEllipsisVertical, FaHelmetSafety, FaStore, FaTrash } from "react-icons/fa6";
 import { useOutletContext } from "react-router-dom";
@@ -13,6 +13,8 @@ import dimensions from "@/data/dimensions.json";
 import modelsServiceInstance from "@/services/modelsServices";
 import { _showToast } from "@/utils/notifications";
 import { ToastDataType } from "@/types/DiversType";
+import templatesServiceInstance from "@/services/TemplatesServices";
+import { AxiosError } from "axios";
 
 interface ContextType {
   shops: ShopType[]
@@ -24,54 +26,28 @@ export default function TemplatePage() {
     
   const {setToastData, toggleShow, } = useOutletContext<ContextType>()
   const [templates, setTemplates] = React.useState<TemplateType[]>([]);
-  const [models, setModels] = React.useState<ModelType[]>([]);
-  const [imageModels, setImageModels] = React.useState<ImagemodelType[]>([]);
-  const [selectedModel, setSelectedModel] = React.useState<ModelType>({} as ModelType);
+  const [selectedModel, setSelectedModel] = React.useState<TemplateType>({} as TemplateType);
   // const [showAddEditModal, setShowAddEditModal] = React.useState<boolean>(false);
   const [showDeleteModal, setShowDeleteModal] = React.useState<boolean>(false);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  // const [filteredModels, setFilteredModels] = React.useState<any[]>([]);
 
 
   React.useEffect(() => {
     _getTemplates(setTemplates);
-    _getModels(setModels);
-    _getAllImagesModels(setImageModels);
+
   }, []);
   React.useEffect(() => {
    
   }, []);
-  // React.useEffect(() => {
-  //   setFilteredModels(
-  //       models.filter((model) =>
-  //         selectedModel.id === model.templateId).map((model) => model.id)
-  //   );
-  // }, [selectedModel]);
 
 
-  // const handleShowAddModal = () => {
-  //   setSelectedModel({} as TemplateType);
-  //   setShowAddEditModal(true);
-  // };
-
-  // const handleShowEditModal = (model: ModelType) => {
-  //   setSelectedModel(model);
-  //   setShowAddEditModal(true);
-  // };
-
-  // const handleCloseAddEditModal = () => {
-  //   setShowAddEditModal(false);
-  //   setSelectedModel({} as ModelType);
-  //   _getTemplates(setTemplates);
-  // };
-
-  const handleShowDeleteModal = (model: ModelType) => {
-    setSelectedModel(model);
+  const handleShowDeleteModal = (temp: TemplateType) => {
+    setSelectedModel(temp);
     setShowDeleteModal(true);
   };
 
   const handleCloseDeleteModal = () => {
-    setSelectedModel({} as ModelType);
+    setSelectedModel({} as TemplateType);
     setShowDeleteModal(false);
   };
 
@@ -79,43 +55,37 @@ export default function TemplatePage() {
     if (!selectedModel) return;
     setIsLoading(true);
     try {
-      const response = await modelsServiceInstance.deleteModel(
+   
+      const response = await templatesServiceInstance.deleteTemplate(
         selectedModel.id,
       );
       handleCloseDeleteModal();
-      _getModels(setModels);
-      _getAllImagesModels(setImageModels);
-
+        _getTemplates(setTemplates);
       _showToast(true, "Suppression réussie", setToastData, toggleShow, 3000);
 
       console.log(response);
-    } catch (err) {
+    } catch (err: unknown) {
       console.error("Erreur lors de la suppression du modèle:", err);
-
+        if(err instanceof AxiosError){
       _showToast(
         false,
-        err instanceof Error
-          ? err.message
+        err.response && err.response.data && err.response.data.message
+          ? err.response.data.message
           : "Erreur lors de la suppression du modèle",
         setToastData,
         toggleShow,
-        3000,
+        5000,
       );
+    }
     } finally {
       setIsLoading(false);
     }
   };
   
-  // const modalAddEditModelProps = { showAddEditModal, handleCloseAddEditModal, selectedModel, setSelectedModel, shopList };
-  // const modalDeleteModelProps = { show: showDeleteModal, handleClose: handleCloseDeleteModal, modelName: selectedModel?.name, handleDelete: handleDeleteModel, isLoading };
 
   return (
     <Container fluid className="relative p-0">
-        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-            <FaHelmetSafety size={150} color="" />
-            Cette page est en construction, revenez plus tard !
-        </div>
-      {/* <Row className="bg-light sticky-top d-flex justify-content-between align-items-center w-100 gx-0 ">
+      <Row className="bg-light sticky-top d-flex justify-content-between align-items-center w-100 gx-0 ">
         <Col xs={2} sm={1} className="pt-2">
         </Col>
         <Col xs={8} sm={10}>
@@ -123,8 +93,8 @@ export default function TemplatePage() {
         </Col>
         <Col xs={2} sm={1}></Col>
       </Row>
-      <Container>
-        {models.length === 0 ? (
+       <Container>
+        {templates.length === 0 ? (
           <div className="text-center py-5">
             <Spinner animation="border" role="status">
               <span className="visually-hidden">Chargement...</span>
@@ -136,41 +106,32 @@ export default function TemplatePage() {
               <tr>
                 <th className="py-3">Id</th>
                 <th className="py-3">Nom</th>
-                <th className="py-3">Dimension</th>
                 <th className="py-3">Miniature</th>
-                <th className="py-3">Magasin</th>
                 <th className="py-3">Actions</th>
               </tr>
             </thead>
             <tbody>
-              {models.map((model, indx) =>{ 
-
-                const templateData = templates &&templates.find((temp) => temp.categoryId === model.categoryId && temp.id === model.templateId );
-                const modelImage = imageModels.find((img) => img.modelId === model.id && img.categoryId === model.categoryId);
-                const dimension = dimensions.find((dim) => dim.id === model.dimensionId);
-                const factor = dimension && 120/dimension?.width
+              {templates.map((temp, indx) =>{ 
                
                 return(
                  <tr key={indx} className="align-middle">
-                  <td>{model.id}</td>
-                  <td>{templateData?.name}</td>
-                  <td>{dimension?.name}</td>
+                  <td>{temp.id}</td>
+                  <td>{temp?.name}</td>
                   <td>
                     <Image
                       src={`${
                         import.meta.env.VITE_API_URL
-                      }/uploads/modelMiniature/${model.id}/${modelImage?.name}`}
-                      alt={`Miniature du model #${model.id}`}
-                      width={dimension && factor && dimension?.width*factor}
-                      height={dimension && factor && dimension?.height*factor}
+                      }/uploads/miniatures/${temp.categoryId}/${temp?.image}`}
+                      alt={`Miniature du temp #${temp.id}`}
+                      width={100}
+                      height={100}
                       />
                   </td>
-                  <td>{model.id}</td>
                   <td>
                     <Dropdown>
                       <Dropdown.Toggle
                         variant='transparent'
-                        id={`dropdown-model-${model.id}`}
+                        id={`dropdown-temp-${temp.id}`}
                         className='border-0 no-chevron'
                       >
                         <b>
@@ -178,15 +139,15 @@ export default function TemplatePage() {
                         </b>
                       </Dropdown.Toggle>
                       <Dropdown.Menu align='end'>
-                        {/* <Dropdown.Item 
+                     {/* <Dropdown.Item 
                         // onClick={}
                           className="d-flex align-items-center"
                           >
                           <FaStore className="me-2" />                          
                           Attribuer un/des magasins ou modifier le nom
-                        </Dropdown.Item>
+                        </Dropdown.Item> */}
                         <Dropdown.Item 
-                        onClick={() => handleShowDeleteModal(model)}
+                        onClick={() => handleShowDeleteModal(temp)}
                           className="d-flex align-items-center text-danger"
                           >
                           <FaTrash className="me-2" size={16} />                          
@@ -200,15 +161,20 @@ export default function TemplatePage() {
             </tbody>
           </Table>
         )}
-      </Container> */}
-  
-      {/* <ModalAddEditModel modalAddEditModelProps={modalAddEditModelProps} /> */}
-      {/* <ModalGenericDelete modalGenericDeleteProps={modalDeleteModelProps} /> */}
+      </Container>
+
         <Modal show={showDeleteModal} onHide={handleCloseDeleteModal}>
         <Modal.Header closeButton>
           <Modal.Title>Supprimer un modèle</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Etes-vous sûr de vouloir supprimer ce modèle ?</Modal.Body>
+        <Modal.Body>Etes-vous sûr de vouloir supprimer ce modèle ?
+            <Alert variant="danger" className="mt-3">
+
+            ⚠️ Ce template est utilisé par des modèles.
+            Voulez-vous vraiment le supprimer ?
+            (Les modèles associés seront également supprimés)
+            </Alert>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseDeleteModal}>
             Annuler
